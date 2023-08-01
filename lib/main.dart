8c2addcca24de9a81c5f50abaef01487c03f0e4c
munavirahammedpk <mnvrahmd313@gmail.com>
2023-08-01 17:16:38 +0530

    bottom nav removed

--- lib/main.dart
+++ lib/main.dart
@@ -23,65 +23,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MainPage(),
+      home: const HomePage(),
     );
   }
 }
 
-class MainPage extends StatelessWidget {
-  const MainPage({super.key});
-
-  final List<Widget> pageList = const [
-    HomePage(),
-    CompletedTaskPage(),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('ToDo List'),
-        elevation: 5,
-      ),
-      body: ValueListenableBuilder(
-        valueListenable: bottomListIndex,
-        builder: (context, newIndx, _) {
-          return pageList[newIndx];
-        },
-      ),
-      floatingActionButton: FloatingActionButton(onPressed: (){
-        Navigator.of(context).push(CupertinoPageRoute(builder: (context){
-          return const AddTaskPage();
-        }));
-      },
-      backgroundColor: Colors.purple,
-      child:const Icon(Icons.add),
-      ),
-      bottomNavigationBar: ValueListenableBuilder(
-          valueListenable: bottomListIndex,
-          builder: (context, newIndex, _) {
-            return BottomNavigationBar(
-              elevation: 5,
-              iconSize: 30,
-              selectedFontSize: 16,
-              selectedItemColor: Colors.purple,
-              items: const [
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.home),
-                  label: 'Home',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.history),
-                  label: 'History',
-                ),
-              ],
-              currentIndex: bottomListIndex.value,
-              onTap: (value) {
-                bottomListIndex.value = value;
-                bottomListIndex.notifyListeners();
-              },
-            );
-          }),
-    );
-  }
-}
